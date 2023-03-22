43dbb2241925029735defb700a40ecbaace9f294
Maxim Prokhorov <prokhorov.max@outlook.com>
2023-03-22 19:10:42 +0300

    different output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <Arduino.h>
+#include <cstdio>
 
 void setup() {
+#if MESSAGE == 1
+    puts("first");
+#elif MESSAGE == 2
+    puts("second");
+#else
+    puts("second");
+#endif
 }
 
 void loop() {
